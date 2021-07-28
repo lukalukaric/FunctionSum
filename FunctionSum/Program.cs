7bc2f334a4784e58349c7a26810dd559106ac14e
Luka Lukarič <luka.lukaric@student.um.si>
2021-07-28 14:44:23 +0200

    implemented function that returns string

--- FunctionSum/Program.cs
+++ FunctionSum/Program.cs
@@ -4,9 +4,72 @@ namespace FunctionSum
 {
     class Program
     {
+        public string FunctionSum(string str)
+        {
+            int sum = 0;
+            string finalString = "";
+            string[] subStrings = str.Split('-');
+            int sumForPluses = 1; // counting for + signs
+
+            foreach (var stringInSubStrings in subStrings)
+            {
+                switch (stringInSubStrings)
+                {
+                    case "zero":
+                        finalString += "0";
+                        break;
+                    case "one":
+                        finalString += "1";
+                        sum += 1;
+                        break;
+                    case "two":
+                        finalString += "2";
+                        sum += 2;
+                        break;
+                    case "three":
+                        finalString += "3";
+                        sum += 3;
+                        break;
+                    case "four":
+                        finalString += "4";
+                        sum += 4;
+                        break;
+                    case "five":
+                        finalString += "5";
+                        sum += 5;
+                        break;
+                    case "six":
+                        finalString += "6";
+                        sum += 6;
+                        break;
+                    case "seven":
+                        finalString += "7";
+                        sum += 7;
+                        break;
+                    case "eight":
+                        finalString += "8";
+                        sum += 8;
+                        break;
+                    case "nine":
+                        finalString += "9";
+                        sum += 9;
+                        break;
+                    default:
+                        return "-1";
+                }
+                if (sumForPluses++ < subStrings.Length)
+                    finalString += "+";
+            }
+            return finalString += ("=" + sum.ToString());
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program prog = new Program();
+
+            Console.WriteLine("Converting one-two-seven-four-nine-zero");
+            Console.WriteLine(prog.FunctionSum("one-two-seven-four-nine-zero") + "\n");
+
+            
         }
     }
 }
